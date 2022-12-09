c6c41256b476e6e2a1c5a1a45b2aac7cabefb80a
Pablo Mochón Ubiña <92191371+Pimubi@users.noreply.github.com>
2022-12-09 08:56:27 +0100

    Agregar archivos de proyecto.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// index.js crear elementos y añadirlos dinamicamente
+
+window.onload = () => {
+	loadModels();
+}
+
+const models = [
+	{
+		material: "red"
+		gps-entity-place: "<37.19210674301847>; longitude: <-3.6170001898254>"
+	}
+]
+
+
+function loadModels ()
+{
+	
+}
